102237ef273e51939ff458c81adb69bec479951d
AustinS910 <smith.3371@wright.edu>
2026-02-11 23:39:33 -0500

    Working more on GUI, changing color of components along with font.

--- src/Calculator.java
+++ src/Calculator.java
@@ -8,7 +8,7 @@ public class Calculator implements ActionListener {
 
     private final JFrame frame;
     private final JPanel panel;
-    JTextField result;
+    private final JTextField result;
 
     public Calculator() {
 
@@ -18,9 +18,20 @@ public class Calculator implements ActionListener {
         panel = new JPanel(new GridBagLayout());
         frame.add(panel);
 
+        panel.setForeground(Color.GRAY);
+        panel.setBackground(Color.GRAY);
+
         GridBagConstraints gbc = new GridBagConstraints();
 
         result = new JTextField("0");
+
+        result.setBackground(Color.gray);
+        result.setForeground(Color.white);
+        result.setFont(new Font("Arial", Font.BOLD, 50));
+        result.setHorizontalAlignment(SwingConstants.RIGHT);
+
+        panel.setForeground(Color.white);
+        panel.setBackground(Color.gray);
 
         gbc.fill = GridBagConstraints.BOTH;
         gbc.gridwidth = GridBagConstraints.REMAINDER;
@@ -28,34 +39,55 @@ public class Calculator implements ActionListener {
         gbc.weighty = 1.0;
         gbc.weightx = 1.0;
 
-        gbc.ipady = 60;
+        gbc.ipady = 70;
         panel.add(result, gbc);
 
         gbc.gridwidth = 1;
         gbc.ipady = 0;
 
-        String[] buttonLabels = { "Del", "Clear", "%", "+", "7", "8", "9", "-", "4", "5", "6", "*", "1", "2", "3",
+        gbc.insets = new Insets(2, 2, 2, 2);
+
+        String[] buttonLabels = { "Del", "Clear", "%", "+", "7", "8", "9", "-", "4", "5", "6", "*", "1", "2", "3", "/",
                 "+/-", "0", ".", "=" };
 
         ArrayList<JButton> buttons = new ArrayList<>();
 
         for (String buttonLabel : buttonLabels) {
+
             JButton button = new JButton(buttonLabel);
+
             button.setActionCommand(buttonLabel);
             button.addActionListener(this);
             buttons.add(button);
         }
 
+        for (JButton button : buttons) {
+
+            button.setBorderPainted(false);
+            button.setOpaque(true);
+            button.setForeground(Color.white);
+
+            button.setFont(new Font("Arial", Font.BOLD, 20));
+
+            if (button.getActionCommand().equals("=")) {
+                button.setBackground(new Color(34, 139, 34));
+
+            } else if ("0123456789".contains(button.getActionCommand())) {
+                button.setBackground(new Color(255, 109, 16));
+
+            } else {
+                button.setBackground(Color.darkGray);
+            }
+        }
         gbc.gridx = 1;
         gbc.gridy = 1;
 
-        gbc.gridwidth = 1;
-
         for (JButton button : buttons) {
-            if (gbc.gridx == 4) {
-                ++gbc.gridy;
+            if (gbc.gridx > 4) {
                 gbc.gridx = 1;
+                ++gbc.gridy;
                 panel.add(button, gbc);
+                ++gbc.gridx;
             } else {
                 panel.add(button, gbc);
                 ++gbc.gridx;
